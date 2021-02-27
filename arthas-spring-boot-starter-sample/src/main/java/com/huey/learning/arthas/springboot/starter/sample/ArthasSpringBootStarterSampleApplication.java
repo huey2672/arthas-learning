e5657cc2046d6d24833b3436b46385db6e3e6429
huey <huey2672@163.com>
2021-02-27 12:10:59 +0800

    Arthas

---
+++ arthas-spring-boot-starter-sample/src/main/java/com/huey/learning/arthas/springboot/starter/sample/ArthasSpringBootStarterSampleApplication.java
@@ -0,0 +1,13 @@
+package com.huey.learning.arthas.springboot.starter.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArthasSpringBootStarterSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArthasSpringBootStarterSampleApplication.class, args);
+    }
+
+}
---
+++ arthas-spring-boot-starter-sample/src/test/java/com/huey/learning/arthas/springboot/starter/sample/ArthasSpringBootStarterSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huey.learning.arthas.springboot.starter.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArthasSpringBootStarterSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
